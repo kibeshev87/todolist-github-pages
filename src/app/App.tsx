6942c038901f6dc711d926df88347690681d0733
kibeshev87 <mrjimb5@gmail.com>
2023-03-03 12:38:19 +0300

    initializationTodolist

---
+++ src/app/App.tsx
@@ -0,0 +1,81 @@
+import React, {useEffect} from 'react'
+import './App.css'
+import {TodolistsList} from '../features/TodolistsList/TodolistsList'
+
+// You can learn about the difference by reading this guide on minimizing bundle size.
+// https://mui.com/guides/minimizing-bundle-size/
+// import { AppBar, Button, Container, IconButton, Toolbar, Typography } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import {Menu} from '@mui/icons-material';
+import LinearProgress from "@mui/material/LinearProgress";
+import {useAppDispatch, useAppSelector} from "./store";
+import {ErrorSnackbar} from "../components/ErrorSnackbar/ErrorSnackbar";
+import {Login} from "../features/Login/Login";
+import {Routes, Route, Navigate} from 'react-router-dom';
+import {initializeAppTC} from "./app-reducer";
+import {CircularProgress} from "@mui/material";
+import {logOutTC} from "../features/Login/authReducer";
+
+
+export function App() {
+
+    const dispatch = useAppDispatch()
+    const status = useAppSelector(state => state.app.status)
+    const isInitialized = useAppSelector(state => state.app.isInitialized)
+    const isLogin = useAppSelector(state => state.auth.isLogin)
+
+
+    useEffect(() => {
+        dispatch(initializeAppTC())
+    }, [])
+
+    if (!isInitialized) {
+        return <div
+            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
+            <CircularProgress/>
+        </div>
+    }
+
+
+    const logoutHandler = () => {
+        dispatch(logOutTC())
+    }
+
+    return (
+        <div className="App">
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+
+                    {isLogin && <Button color="inherit" onClick={logoutHandler}>Logout</Button>}
+
+                </Toolbar>
+            </AppBar>
+
+            {status === 'loading' && <LinearProgress color="secondary"/>}
+
+            <Container fixed>
+
+                <Routes>
+                    <Route path='/' element={<TodolistsList/>}/>
+                    <Route path='login' element={<Login/>}/>
+                    <Route path='404' element={<h1 style={{textAlign: 'center'}}>404:PAGE NOT FOUND</h1>}/>
+                    <Route path='*' element={<Navigate to={'404'}/>}/>
+                </Routes>
+
+            </Container>
+
+            <ErrorSnackbar/>
+        </div>
+    )
+}
---
+++ src/components/AddItemForm/AddItemForm.tsx
@@ -0,0 +1,53 @@
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import { AddBox } from '@mui/icons-material';
+
+type AddItemFormPropsType = {
+    addItem: (title: string) => void
+    isDisabled?: boolean
+}
+
+export const AddItemForm = React.memo(function (props: AddItemFormPropsType) {
+    console.log('AddItemForm called')
+
+    let [title, setTitle] = useState('')
+    let [error, setError] = useState<string | null>(null)
+
+    const addItem = () => {
+        if (title.trim() !== '') {
+            props.addItem(title);
+            setTitle('');
+        } else {
+            setError('Title is required');
+        }
+    }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (error !== null) {
+            setError(null);
+        }
+        if (e.key === 'Enter') {
+            addItem();
+        }
+    }
+
+    return <div>
+        <TextField variant="outlined"
+                   error={!!error}
+                   value={title}
+                   onChange={onChangeHandler}
+                   onKeyPress={onKeyPressHandler}
+                   label="Title"
+                   helperText={error}
+                   disabled={props.isDisabled}
+        />
+        <IconButton color="primary" onClick={addItem} disabled={props.isDisabled} >
+            <AddBox/>
+        </IconButton>
+    </div>
+})
